08f7ca742fe354a5fdfdaf2c96447c74b005adef
Gabriel Borchardt <gabriel.borchardt@millpar.com>
2023-11-23 08:39:07 -0300

    Updating cors config for api

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const jsonServer = require('json-server');
+const express = require('express');
+const cors = require('cors');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -6,8 +8,11 @@ const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
 
+server.use(cors());
+
 server.use(middlewares);
 server.use(router);
-server.listen(port, ()=>{
-    console.log(`JSON Server is running in ${port}`);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running in ${port}`);
 });
